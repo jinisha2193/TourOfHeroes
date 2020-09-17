a6dacf0c0961f23df441dd84e69864cb022e9707
jinisha2193@gmail.com <jinisha2193@gmail.com>
2020-09-17 18:53:18 -0500

    finished part 2

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../heroes';
+import { Hero } from '../hero';
+import { heroes } from '../mockData/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  selectedHero: Hero;
+  heroes = heroes;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-
-  hero: Hero = {
-    id: 1,
-    name: 'windstorm'
-  }
-
 }
