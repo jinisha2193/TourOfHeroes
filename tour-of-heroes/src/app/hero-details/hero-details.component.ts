d15591b26187e5e84ddc0bfc894f50d9888ab1b3
jinisha2193@gmail.com <jinisha2193@gmail.com>
2020-09-17 19:06:06 -0500

    finished part 3

---
+++ tour-of-heroes/src/app/hero-details/hero-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-details',
+  templateUrl: './hero-details.component.html',
+  styleUrls: ['./hero-details.component.css'],
+})
+export class HeroDetailsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  @Input() hero: Hero;
+}
